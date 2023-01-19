ce38aa6ebc2192ef23680bc5b1c0da2da22830b1
Sabah Ummie <sabah.ummie@gmail.com>
2023-01-19 16:51:13 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+// IMperative
+// const h1 = document.createElement('h1');
+// h1.textContent = 'This is an imperative way to program';
+// h1.className = 'header';
+// document.getElementById('root').append(h1);
+
+// Declarative
+ReactDOM.render(<h1 className="header">Hello World</h1>, document.getElementById('root'));
