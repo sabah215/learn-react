5b8623a36a5d9b9d442ae039dc6a99f12b8fbf31
Sabah Ummie <sabah.ummie@gmail.com>
2023-01-24 21:54:53 -0500

    react component implemented and some styling done

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
-function ReasonList(){
-       return(
-           <div>
-               <ol>
-                   <li>The component based development helps make code more manageable</li>
-                   <li>The JSX syntax are not complext at all to learn</li>
-                   <li>The react community is strong and responsive enough to solve problems</li>
-               </ol>
-           </div>
-       );
-   }
-   
+function Header() {
+    return (
+        <header>
+            <nav className="nav">
+                <img className="react-logo" src="./images/react-logo.png"  />
+                <ul className="nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                 </ul>
+            </nav>
+        </header>
+    )
+}
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <small>© 2023 Ummie development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function MainContent() {
+    return (
+        <div className="main-content">
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
+            </ol>
+        </div>
+    )
+}
+
+function Page() {
+    return (
+        <div className= "page-container">
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
+}
    ReactDOM.render(
-       <ReasonList/>,
+       <Page />,
        document.getElementById('root')
    );
