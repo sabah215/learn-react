461afacdfabe5f294146aa1f6ad1ccb96abde9f6
Sabah Ummie <sabah.ummie@gmail.com>
2023-01-26 21:34:23 -0500

    back to old structure

---
+++ Header.js
@@ -0,0 +1,15 @@
+export default function Header() {
+       return (
+           <header>
+               <nav className="nav">
+                   <img className="react-logo" src="./images/react-logo.png"  />
+                   <ul className="nav-items">
+                       <li>Pricing</li>
+                       <li>About</li>
+                       <li>Contact</li>
+                    </ul>
+               </nav>
+           </header>
+       )
+   }
+
--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-function Header() {
-    return (
-        <header>
-            <nav className="nav">
-                <img className="react-logo" src="./images/react-logo.png"  />
-                <ul className="nav-items">
-                    <li>Pricing</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                 </ul>
-            </nav>
-        </header>
-    )
-}
+import Header from "./Header.js"
+
 
 function Footer() {
     return (
